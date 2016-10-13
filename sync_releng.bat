a03937943879a34413115c24a0712a0ad88c1b3c
Wuyang Chung <wuyang.m.chung@gmail.com>
2016-10-14 00:12:08 +0800

    Add a batch file to sync with upstream.

---
+++ sync_releng.bat
@@ -0,0 +1,10 @@
+echo "$ git fetch upstream"
+git fetch upstream
+#
+#git checkout releng/11.0
+echo
+echo "$ git merge upstream/releng/11.0"
+git merge upstream/releng/11.0
+echo
+echo "*** NOTE *** Use GitSmart to sync commits to origin"
+
