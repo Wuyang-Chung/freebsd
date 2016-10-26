e2b59d08b8990c2dc69372d77e8cce664fd45b30
Wuyang Chung <wuyang.m.chung@gmail.com>
2016-10-26 12:24:03 +0800

    1. Add sync_rebase.bat. 2. Rename sync_releng.bat to sync_merge.bat

---
+++ sync_rebase.bat
@@ -0,0 +1,8 @@
+echo "$ git fetch upstream"
+git fetch upstream
+echo
+echo "$ git rebase upstream/releng/11.0"
+git rebase upstream/releng/11.0
+echo
+echo "*** NOTE *** Use GitSmart to sync commits to origin"
+
