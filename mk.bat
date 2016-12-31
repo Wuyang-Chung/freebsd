ebb01ae1a21d0ffe893d6e0b7a47d620646ca776
Wuyang Chung <wuyang.m.chung@gmail.com>
2016-12-31 21:06:38 +0800

    Souce code directory renamed.

--- mk.bat
+++ mk.bat
@@ -1,7 +1,7 @@
 #!/bin/csh
 make kernel KERNCONF=WYCVM
 if ($? == 0) then
-	cp /usr/obj/nfs/freebsd-vm/sys/WYCVM/kernel.debug 0_wyc
+	cp /usr/obj/nfs/vm.FreeBSD/sys/WYCVM/kernel.debug 0_wyc
 	if ($? == 0) then
 		reboot
 	endif
