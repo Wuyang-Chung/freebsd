879d179b5314b033e5f0a126203db38882d341df
Wuyang Chung <wuyang.m.chung@gmail.com>
2017-01-03 09:58:13 +0800

    Echo 2 lines when kernel copying is done.

--- mk.bat
+++ mk.bat
@@ -3,6 +3,8 @@ make kernel KERNCONF=WYCVM
 if ($? == 0) then
 	cp /usr/obj/nfs/vm.FreeBSD/sys/WYCVM/kernel.debug 0_wyc
 	if ($? == 0) then
+		echo "*** kernel copied"
+		echo "*** rebooting the machine ...."
 		reboot
 	endif
 endif
