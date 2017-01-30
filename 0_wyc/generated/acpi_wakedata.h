6ba7f03a896ef415112aa9b3cc0952bbaba8ad5b
Wuyang Chung <wuyang.m.chung@gmail.com>
2017-01-30 20:19:49 +0800

    Add the files generated during build.

---
+++ 0_wyc/generated/acpi_wakedata.h
@@ -0,0 +1,16 @@
+#define	wakeup_start	0x00000000
+#define	wakeup_sw32	0x0000006e
+#define	wakeup_32	0x00000078
+#define	resume_beep	0x000000ad
+#define	reset_video	0x000000ae
+#define	bootgdt	0x000000b0
+#define	bootcode32	0x000000b8
+#define	bootdata32	0x000000c0
+#define	bootgdtdesc	0x000000c8
+#define	bootgdtend	0x000000c8
+#define	wakeup_cr4	0x000000d0
+#define	wakeup_cr3	0x000000d4
+#define	wakeup_pcb	0x000000d8
+#define	wakeup_ret	0x000000dc
+#define	wakeup_gdt	0x000000e0
+#define	dummy	0x000000e6
