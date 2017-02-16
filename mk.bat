8f2948f9153ffe6b29cbcd4db702edea61ecfe10
Wuyang Chung <wuyang.m.chung@gmail.com>
2017-02-16 18:06:24 +0800

    Reboot the system anyway.

--- mk.bat
+++ mk.bat
@@ -4,8 +4,8 @@ if ($? == 0) then
 	cp /usr/obj/nfs/vm.FreeBSD/sys/WYCVM/kernel.debug 0_wyc
 	if ($? == 0) then
 		echo "*** kernel copied"
-		echo "*** rebooting the machine ...."
-		reboot
 	endif
+	echo "*** rebooting the machine ...."
+	reboot
 endif
 
