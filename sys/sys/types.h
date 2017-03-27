f0615928ba5e6ed0733916369ba68b76a0ceebae
Wuyang Chung <wuyang.m.chung@outlook.com>
2017-03-27 14:28:46 +0800

    Add comment.

--- sys/sys/types.h
+++ sys/sys/types.h
@@ -250,7 +250,7 @@ typedef	struct cap_rights	cap_rights_t;
 #endif
 
 typedef	__vm_offset_t	vm_offset_t;
-typedef	__vm_ooffset_t	vm_ooffset_t;
+typedef	__vm_ooffset_t	vm_ooffset_t; //wyc: why __int64_t???
 typedef	__vm_paddr_t	vm_paddr_t;
 typedef	__vm_pindex_t	vm_pindex_t;
 typedef	__vm_size_t	vm_size_t;
