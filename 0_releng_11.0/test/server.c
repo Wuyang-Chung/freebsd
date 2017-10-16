b6d6637526c5f0797b0329750e503c6b28c42eee
Wuyang Chung <wuyang.m.chung@outlook.com>
2017-10-16 21:10:36 +0800

    Add cpu_bind().

--- 0_releng_11.0/test/server.c
+++ 0_releng_11.0/test/server.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <err.h>
+#include <sys/param.h>
+#include <sys/cpuset.h>
 #include <sys/stat.h>
+
+void cpu_bind(void)
+{
+	int i;
+	cpuset_t myset;
+
+	/* Get CPU mask for the current thread */
+	if (cpuset_getaffinity(CPU_LEVEL_WHICH, CPU_WHICH_TID, -1, sizeof(myset), &myset) == -1)
+		err(1, "getaffinity failed");
+
+	/* Find first available CPU - don't assume CPU0 is always available */
+	for (i = 0; i < CPU_SETSIZE; i++)
+		if (CPU_ISSET(i, &myset))
+			break; 
+
+	if (i == CPU_SETSIZE)
+		err(1, "Not allowed to run on any CPUs?  How did I print this, then?");
+
+	/* Set new CPU mask */
+	printf ("Setting affinity to CPU %d\n", i);
+	CPU_ZERO(&myset);
+	CPU_SET(i, &myset);
+
+	if (cpuset_setaffinity(CPU_LEVEL_WHICH, CPU_WHICH_TID, -1, sizeof(myset), &myset) == -1)
+		err(1, "setaffinity failed");
+}
 
 char const * const fifo = "/tmp/fifo";
 
@@ -10,6 +39,7 @@ int main(int argc, char *argv[])
 	int	fh;
 	unsigned char	c;
 
+	cpu_bind();
 	mkfifo(fifo, 0600);
 	fh = open(fifo, O_RDWR);
 	while(1) {
